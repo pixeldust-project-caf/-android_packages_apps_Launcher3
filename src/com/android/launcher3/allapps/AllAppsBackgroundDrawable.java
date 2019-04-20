589952af9521c6e2935e613b4ffae1dcb72c0d70
spezi77 <spezi7713@gmx.net>
2019-04-20 18:06:35 +0200

    Revert "Add support for black theme(s)."
    This reverts commit fdf45afb2cfd68c3d6ffdf494947414cc2ac54a0.

--- src/com/android/launcher3/allapps/AllAppsBackgroundDrawable.java
+++ src/com/android/launcher3/allapps/AllAppsBackgroundDrawable.java
@@ -110,20 +110,11 @@ public class AllAppsBackgroundDrawable extends Drawable {
 
         final int systemTheme = Settings.System.getInt(context.getContentResolver(), SYSTEM_THEME, 0);
          switch (systemTheme) {
-            case 1: /*Light*/
+            case 1:
                 context = new ContextThemeWrapper(context, R.style.AllAppsEmptySearchBackground);
                 break;
-            case 2: /*Dark*/
+            case 2:
                 context = new ContextThemeWrapper(context, R.style.AllAppsEmptySearchBackground_Dark);
-                break;
-            case 3: /*Black*/
-                context = new ContextThemeWrapper(context, R.style.AllAppsEmptySearchBackground_Black);
-                break;
-            case 4: /*ShishuNights*/
-                context = new ContextThemeWrapper(context, R.style.AllAppsEmptySearchBackground_Black);
-                break;
-            case 5: /*Chocolate*/
-                context = new ContextThemeWrapper(context, R.style.AllAppsEmptySearchBackground_Black);
                 break;
             default:
                 context = new ContextThemeWrapper(context, Themes.getAttrBoolean(context, R.attr.isMainColorDark)
