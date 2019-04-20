CarbonROM/android_packages_apps_Launcher3

Fruity Pebbles v2.0
Change-Id: I8a18d6621415c41ed4076268245801fefe1b49d2

src/com/android/launcher3/BaseDraggingActivity.java
@@ -17,8 +17,8 @@
 package com.android.launcher3;
 
 import android.app.ActivityOptions;
+import android.app.UiModeManager;
 import android.content.ActivityNotFoundException;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
@@ -27,10 +27,8 @@ import android.os.Bundle;
 import android.os.Process;
 import android.os.StrictMode;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.ActionMode;
-import android.view.Surface;
 import android.view.View;
 import android.widget.Toast;
 
@@ -58,8 +56,6 @@ public abstract class BaseDraggingActivity extends BaseActivity
     // automatically when user interacts with the launcher.
     public static final Object AUTO_CANCEL_ACTION_MODE = new Object();
 
-    private static final String SYSTEM_THEME = "system_theme";
-
     private ActionMode mCurrentActionMode;
     protected boolean mIsSafeModeEnabled;
 
@@ -68,6 +64,8 @@ public abstract class BaseDraggingActivity extends BaseActivity
     private int mThemeRes = R.style.LauncherTheme;
 
     private DisplayRotationListener mRotationListener;
+
+    private UiModeManager mUiModeManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +81,7 @@ public abstract class BaseDraggingActivity extends BaseActivity
             mThemeRes = themeRes;
             //setTheme(themeRes);
         }
+        mUiModeManager = this.getSystemService(UiModeManager.class);
         updateTheme(wallpaperColorInfo);
     }
 
@@ -278,20 +277,15 @@ public abstract class BaseDraggingActivity extends BaseActivity
         void onActivityStart(T activity);
     }
 
-    protected void updateTheme(WallpaperColorInfo wallpaperColorInfo) {
-        ContentResolver resolver = this.getContentResolver();
-        final boolean supportsDarkText = wallpaperColorInfo.supportsDarkText();
-        final int systemTheme = Settings.System.getInt(resolver, SYSTEM_THEME, 0);
-        switch (systemTheme) {
-            case 1:
-                setTheme(supportsDarkText ? R.style.LauncherTheme_DarkText : R.style.LauncherTheme);
-                break;
-            case 2:
-                setTheme(supportsDarkText ? R.style.LauncherThemeDark_DarKText : R.style.LauncherThemeDark);
-                break;
-            default:
-                setTheme(mThemeRes);
-                break;
+    private void updateTheme(WallpaperColorInfo wallpaperColorInfo) {
+        if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_AUTO) {
+            setTheme(wallpaperColorInfo.supportsDarkText() ? R.style.LauncherTheme_DarkText :
+                    R.style.LauncherTheme);
+        } else if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES) {
+            setTheme(wallpaperColorInfo.supportsDarkText() ? R.style.LauncherThemeDark_DarKText :
+                    R.style.LauncherThemeDark);
+        } else {
+            setTheme(mThemeRes);
         }
     }
 }

pixeldust-project-caf/-android_packages_apps_Launcher3

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/launcher3/BaseDraggingActivity.java
@@ -17,8 +17,8 @@
 package com.android.launcher3;
 
 import android.app.ActivityOptions;
+import android.app.UiModeManager;
 import android.content.ActivityNotFoundException;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
@@ -27,10 +27,8 @@ import android.os.Bundle;
 import android.os.Process;
 import android.os.StrictMode;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.ActionMode;
-import android.view.Surface;
 import android.view.View;
 import android.widget.Toast;
 
@@ -58,8 +56,6 @@ public abstract class BaseDraggingActivity extends BaseActivity
     // automatically when user interacts with the launcher.
     public static final Object AUTO_CANCEL_ACTION_MODE = new Object();
 
-    private static final String SYSTEM_THEME = "system_theme";
-
     private ActionMode mCurrentActionMode;
     protected boolean mIsSafeModeEnabled;
 
@@ -68,6 +64,8 @@ public abstract class BaseDraggingActivity extends BaseActivity
     private int mThemeRes = R.style.AppTheme;
 
     private DisplayRotationListener mRotationListener;
+
+    private UiModeManager mUiModeManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +81,7 @@ public abstract class BaseDraggingActivity extends BaseActivity
             mThemeRes = themeRes;
             //setTheme(themeRes);
         }
+        mUiModeManager = this.getSystemService(UiModeManager.class);
         updateTheme(wallpaperColorInfo);
     }
 
@@ -278,20 +277,15 @@ public abstract class BaseDraggingActivity extends BaseActivity
         void onActivityStart(T activity);
     }
 
-    protected void updateTheme(WallpaperColorInfo wallpaperColorInfo) {
-        ContentResolver resolver = this.getContentResolver();
-        final boolean supportsDarkText = wallpaperColorInfo.supportsDarkText();
-        final int systemTheme = Settings.System.getInt(resolver, SYSTEM_THEME, 0);
-        switch (systemTheme) {
-            case 1:
-                setTheme(supportsDarkText ? R.style.LauncherTheme_DarkText : R.style.LauncherTheme);
-                break;
-            case 2:
-                setTheme(supportsDarkText ? R.style.LauncherThemeDark_DarKText : R.style.LauncherThemeDark);
-                break;
-            default:
-                setTheme(mThemeRes);
-                break;
+    private void updateTheme(WallpaperColorInfo wallpaperColorInfo) {
+        if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_AUTO) {
+            setTheme(wallpaperColorInfo.supportsDarkText() ? R.style.LauncherTheme_DarkText :
+                    R.style.LauncherTheme);
+        } else if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES) {
+            setTheme(wallpaperColorInfo.supportsDarkText() ? R.style.AppTheme_DarkText :
+                    R.style.LauncherTheme_Dark);
+        } else {
+            setTheme(mThemeRes);
         }
     }
 }