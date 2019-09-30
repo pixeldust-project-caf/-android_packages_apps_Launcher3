13a417a8acd518913282767d7201641adfd47e5f
Chris Crump <chris@cypheros.co>
2019-09-29 21:17:44 -0400

    Launcher3: Create Custom Launcher [spezi77] Debranded to com.pixeldust.launcher Change-Id: Ic85cea0eb2d00b7db7753bb509cff521d84e2ffc Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ src/com/pixeldust/launcher/PixeldustLauncher.java
@@ -0,0 +1,14 @@
+/*
+ *
+ *   Copyright (C) 2023 The PixelDust Project
+ *   SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.pixeldust.launcher;
+
+import com.android.launcher3.Launcher;
+
+public class PixeldustLauncher extends Launcher {
+
+}
