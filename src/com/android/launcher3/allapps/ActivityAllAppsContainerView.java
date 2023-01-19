31de7b4822b328362bace427ac7594fe6ac7e3ec
Pranav Vashi <neobuddy89@gmail.com>
2023-01-19 20:29:44 +0530

    Launcher3: Match app searchbar theme with dock searchbar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -276,11 +276,6 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
         updateBackgroundVisibility(mActivityContext.getDeviceProfile());
         if (Utilities.showSearch(getContext())) {
             mSearchContainer.setVisibility(View.VISIBLE);
-            if (!Utilities.isThemedIconsEnabled(getContext())) {
-                getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google);
-            } else {
-                getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google_themed);
-            }
         } else {
             mSearchContainer.setVisibility(View.GONE);
         }
@@ -688,11 +683,6 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
         if (mTabsProtectionAlpha != tabsAlpha) {
             mTabsProtectionAlpha = tabsAlpha;
             invalidateHeader();
-        }
-        if (!Utilities.isThemedIconsEnabled(getContext())) {
-            getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google);
-        } else {
-            getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google_themed);
         }
         if (mSearchUiManager.getEditText() == null) {
             return;
